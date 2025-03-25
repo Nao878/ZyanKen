f66b4c1e48f5655d118c2a174eaa0bb4d104d7ee
Nao878 <yanaoto220@gmail.com>
2025-03-25 23:40:01 +0900

    basic program add

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Button : MonoBehaviour
 {
-    public GameObject correct;
+    public TMP_Text resultText,cpuHandText;
+    private string[] hands = { "グー", "チョキ", "パー" };
 
-    public void Press()
+    public void PlayerChoice(int playerHand)
     {
-        correct.SetActive(true);
+        int cpuHand = Random.Range(0, 3);
+        cpuHandText.text = "CPU: " + hands[cpuHand];
+
+        string result = DetermineWinner(playerHand, cpuHand);
+        resultText.text = result;
+    }
+
+    private string DetermineWinner(int playerHand, int cpuHand)
+    {
+        if (playerHand == cpuHand)
+            return "引き分け";
+        else if ((playerHand == 0 && cpuHand == 1) || (playerHand == 1 && cpuHand == 2) || (playerHand == 2 && cpuHand == 0))
+            return "あなたの勝ち";
+        else
+            return "あなたの負け...";
     }
 }
